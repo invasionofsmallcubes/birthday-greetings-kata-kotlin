6b3327e1a5b1eaaa42983e3184591b876aeafdb4
Emanuele Ianni <dierre@gmail.com>
2017-06-17 16:18:26 +0200

    iteration #7

--- src/main/kotlin/it/xpug/kata/birthday_greetings/XDate.kt
+++ src/main/kotlin/it/xpug/kata/birthday_greetings/XDate.kt
@@ -3,28 +3,18 @@ package it.xpug.kata.birthday_greetings
 import java.text.SimpleDateFormat
 import java.util.*
 
-class XDate(yyyyMMdd: String) {
+data class XDate(val date: Date) {
 
-    private var date: Date = SimpleDateFormat("yyyy/MM/dd").parse(yyyyMMdd)
+    constructor(yyyyMMdd: String) : this(SimpleDateFormat("yyyy/MM/dd").parse(yyyyMMdd))
 
-    val day: Int
+    val day
         get() = getPartOfDate(GregorianCalendar.DAY_OF_MONTH)
 
-    val month: Int
+    val month
         get() = 1 + getPartOfDate(GregorianCalendar.MONTH)
 
     fun isSameDay(anotherDate: XDate): Boolean {
         return anotherDate.day == this.day && anotherDate.month == this.month
-    }
-
-    override fun hashCode(): Int {
-        return date.hashCode()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other !is XDate)
-            return false
-        return other.date == this.date
     }
 
     private fun getPartOfDate(part: Int): Int {
@@ -32,5 +22,4 @@ class XDate(yyyyMMdd: String) {
         calendar.time = date
         return calendar.get(part)
     }
-
 }
